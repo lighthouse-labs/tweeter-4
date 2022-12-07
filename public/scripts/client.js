1a26962be429f13128a4c8ed383b6ec051819b54
Njoe00 <nick.james.joe@gmail.com>
2022-12-07 22:16:06 +0000

    updating creatTweetElement variable names to be more speific

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,11 +6,11 @@
 // $(document).ready(function() {
 
   const createTweetElement = (tweetData) => {
-    tweetTemp1 = (`<article class="tweet">${tweetData.content.text}</article>\n`);
-    tweetTemp2 = (`<div class ="article-tweet-header-profile">${tweetData.avatars}>\n`);
-    tweetTemp3 = (`<p>${tweetData.user.name}</p>\n`);
-    tweetTemp4 = (`<div class="tweet-handle">${tweetData.user.handle}</div>\n`);
-    tweetTemp5 = (`<div class="tweet-post-date">${tweetData.created_at}</div>\n`);
+    const userTweets = (`<article class="tweet">${tweetData.content.text}</article>\n`);
+    const userAvatar = (`<div class ="article-tweet-header-profile">${tweetData.avatars}>\n`);
+    const userName = (`<p>${tweetData.user.name}</p>\n`);
+    const userHandle = (`<div class="tweet-handle">${tweetData.user.handle}</div>\n`);
+    const tweetDate = (`<div class="tweet-post-date">${tweetData.created_at}</div>\n`);
     
     console.log(tweetTemp1, tweetTemp2, tweetTemp3, tweetTemp4, tweetTemp5)
     
