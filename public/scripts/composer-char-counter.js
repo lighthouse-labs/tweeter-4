0528f29c2b5399877a2d14300da5afdab90d7c70
Njoe00 <nick.james.joe@gmail.com>
2022-12-07 01:01:51 +0000

    using eslint to fix formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,20 @@
 
 $(document).ready(function() {
 
-    
-    $('#tweet-text').on("input", function () {
-        const max = 140;
-        const charCount = $(this).val().length;
-        let counting = max - charCount;
-        const counter = $(this).parent().find(".counting");
+  $('#tweet-text').on("input", function() {
+    const max = 140;
+    const charCount = $(this).val().length;
+    let counting = max - charCount;
+    const counter = $(this).parent().find(".counting");
         
-        counter.text(counting)
+    counter.text(counting);
 
-        if (counting < 0 ) {
-            counter.css('color', 'red');
-          } else {
-            counter.css('color', '#545454');
-        }
-    });
+    if (counting < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', '#545454');
+    }
+  });
     
 });
 
