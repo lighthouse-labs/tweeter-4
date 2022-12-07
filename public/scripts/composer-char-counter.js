b73b8f9c8406bded36c4ec0e69426f0027286e19
Njoe00 <nick.james.joe@gmail.com>
2022-12-07 00:38:57 +0000

    creating max chac count - current count for text box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 
     
     $('#tweet-text').on("input", function () {
+        const max = 140;
         const charCount = $(this).val().length;
-        console.log(charCount);
-       const counter = $(this).parent().find(".counter");
-        counter.()
+        const counting = max - charCount;
+        
+        const counter = $(this).parent().find(".counter");
+        console.log(counting);
     })
     
 });
