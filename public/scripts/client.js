d3f9fbd328ff0078a83a40da23e272bcc13c1f3a
Njoe00 <nick.james.joe@gmail.com>
2022-12-07 22:58:57 +0000

    updating createTweetElement to return new hmtl string with dynamic variable that can access objecs key:pair values and renders onto tweeterapp

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,36 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// $(document).ready(function() {
+$(document).ready(function() {
 
   const createTweetElement = (tweetData) => {
-    const userTweets = (`<article class="tweet">${tweetData.content.text}</article>\n`);
-    const userAvatar = (`<div class ="article-tweet-header-profile">${tweetData.avatars}>\n`);
-    const userName = (`<p>${tweetData.user.name}</p>\n`);
-    const userHandle = (`<div class="tweet-handle">${tweetData.user.handle}</div>\n`);
-    const tweetDate = (`<div class="tweet-post-date">${tweetData.created_at}</div>\n`);
+    const userTweetText = (`<article class="tweet">${tweetData.content.text}</article>`);
+    const userAvatar = (`<div class ="article-tweet-header-profile">${tweetData.avatars}>`);
+    const userName = (`<p>${tweetData.user.name}</p>`);
+    const userHandle = (`<div class="tweet-handle">${tweetData.user.handle}</div>`);
+    const tweetDate = (`<div class="tweet-post-date"></div>`);
     
-    console.log(tweetTemp1, tweetTemp2, tweetTemp3, tweetTemp4, tweetTemp5)
-    
+    const userTweets = `<article class="article">
+  <header class="article-tweet-header">
+    <div class="article-tweet-header-profile">
+      <div class="tweet-header-icon">
+        <img src= ${tweetData.avatars}">
+      </div>  
+      <p>${tweetData.user.name}</p>
+    </div>
+    <div class="tweet-handle">${tweetData.user.handle}</div>
+    </header>
+    <p class="tweet-text">${tweetData.content.text}</p>
+  <footer class="article-tweet-footer">
+    <div class="tweet-post-date">${tweetData.created_at}</div>
+    <div class="tweet-footer-icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-sharp fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+</article>`
+    return userTweets;
   };
   
   const tweetData = {
@@ -29,28 +48,6 @@
  }
  
  const $tweet = createTweetElement(tweetData);
-//  console.log($tweet);
-//  $('#tweets-container').append($tweet); 
-// });
-
-{/* <section id="tweets-container">
-<article class="article">
-  <header class="article-tweet-header">
-    <div class="article-tweet-header-profile">
-      <div class="tweet-header-icon">
-        <img src= "https://i.imgur.com/73hZDYK.png">
-      </div>  
-      <p>Newton</p>
-    </div>
-    <div class ="tweet-handle">@SirIsaac</div>
-  </header>
-    <p class="tweet-text">"If I have seen further it is standing by on shoulders of giants"</p>
-  <footer class="article-tweet-footer">
-    <div class="tweet-post-date">10 days ago</div>
-    <div class="tweet-footer-icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-sharp fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-  </footer>
-</article> */}
+ console.log($tweet);
+ $('#tweets-container').append($tweet); 
+});
