ec3fb5b19d3692b7d6544dffed681c6467944fd8
Njoe00 <nick.james.joe@gmail.com>
2022-12-09 03:45:57 +0000

    fixing formatting with eslint and adding comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,21 @@
 
 $(document).ready(function() {
-
+  // setting listener for input on tweet-text text-box
   $('#tweet-text').on("input", function() {
     const max = 140;
+    // assigning length of characters inside tweet-text box
     const charCount = $(this).val().length;
+    // setting the countter for the max number of chars vs the current value of the textbook string length
     let counting = max - charCount;
+    // assigning the class 'counting' from index.html
     const counter = $(this).parent().find(".counting");
-        
+    //applying the display for counting classs to our couting function
     counter.text(counting);
-
+    // if the char limit is reached the numbers of the counter will turn red
     if (counting < 0) {
       counter.css('color', 'red');
     } else {
+      // if the character count goes above 0 it will return back to black
       counter.css('color', '#545454');
     }
   });
