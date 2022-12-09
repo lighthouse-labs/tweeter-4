0da8952207412f9bc0d1038961ab251e41ae6a4d
Njoe00 <nick.james.joe@gmail.com>
2022-12-09 03:37:14 +0000

    updating formatting to be cleaner

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,14 @@ $(document).ready(function() {
   // takes in tweeter data objects and as 
   const renderTweets = function (tweets) {
     //reverses tweets object array order
+
     let reversedTweets = tweets.reverse();
-    //iterates over it calls createTweetElement 
+    //iterates over it calls createTweetElement
     for (let key in reversedTweets) {
+
       // calls createTweetElement as acallback function to attach key pair values from object
       const $tweet = createTweetElement(reversedTweets[key]);
+
       // apends html template to tweets-container in index.html 
       $('#tweets-container').append($tweet);     
     }
@@ -44,27 +47,36 @@ $(document).ready(function() {
 
   // listens for submissions from #tweet-form 
   $("#tweet-form").submit(function (event) {
+
     event.preventDefault();
     // parses data from tweet-form into human-readable text
     let parseData = ($(this).serialize());
+
     // represnts value of tweet-texts 
     const tweetText = $('#tweet-text').val();
+    
     // if statement to confirm that the user has none of the error checks below and hides error message
     if (tweetText.length !== 0 || tweetText.length !== null || tweetText.length > -1) {
      $('#new-tweet-err-message').hide().text();
+
     }
     // if the length of the user tweet is greater 140 characters returns error message
     if (tweetText.length > 140) {
        $('#new-tweet-err-message').text("You've exceed the characer limit!").slideDown(750);
+
     // if the length of the users tweet is greater 140 characters returns error message
     } else if (tweetText.length === 0 || tweetText.length === null) {
         $('#new-tweet-err-message').text("Your Tweet box is empty!").slideDown(750);
+
     } else {
-      // makes ajax post request to tweets and takes server data response and calls the loadTweets function
+      // makes ajax post request to tweets 
       $.ajax ({
         type: "POST",
         url: "/tweets",
+        //takes server data response 
         data: parseData,
+        
+        // calls the loadTweets function
         success: function (data) {
           loadTweets();
         }
@@ -74,12 +86,15 @@ $(document).ready(function() {
 
   const loadTweets = function  () {
     //requests JSON data from '/tweets' route 
+
     $.getJSON ({
       url: "/tweets",
       success: function (data) {
         // empties any existing data from previous tweets 
+
         $('#tweets-container').empty();
         //  and calls renderTweets function with JSON data taken from '/tweets' route
+
         renderTweets(data);
       }
     })
